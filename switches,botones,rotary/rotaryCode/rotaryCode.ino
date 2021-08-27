9e37d74af11f58abc49b51c34652f51699a4784c
Tomas Molino <59565479+totomolino@users.noreply.github.com>
2021-08-27 13:03:09 -0300

    Agregue una partecita del rotary

---
+++ switches,botones,rotary/rotaryCode/rotaryCode.ino
@@ -0,0 +1,16 @@
+#include <Keypad.h>
+#include <Joystick.h>
+
+int pinA = 1;
+
+int pinB = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
